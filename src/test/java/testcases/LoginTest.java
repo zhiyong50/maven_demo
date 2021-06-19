d779decda63439646122b2a56c1ea9b93c71a2b5
“xzy” <824838674@qq.com>
2021-06-19 22:48:52 +0800

    first commit

---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+/**
+ * @Project: jenkins_test
+ * @Site: http://www.lemonban.com
+ * @Forum: http://testingpai.com
+ * @Copyright: ©2021 柠檬班Java28期
+ * @Author: Administrator
+ * @Create: 2021-06-19 18:21
+ * @Desc：
+ **/
+public class LoginTest {
+    @Test
+    public void loginSuccess(){
+        System.out.println("登录成功");
+    }
+}
